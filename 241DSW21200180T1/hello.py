434f2879861aee221a04803d3d700168f9b9fef8
JoArDiTo-DEV <sm.armando05@gmail.com>
2024-04-10 13:00:46 -0500

    3a

--- 241DSW21200180T1/hello.py
+++ 241DSW21200180T1/hello.py
@@ -1,17 +1,20 @@
-#COMMIT 2b
+#COMMIT 3a
 
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
-#EL SIGUIENTE COMMIT ES EL 3a
+#EL SIGUIENTE COMMIT ES EL 3b
