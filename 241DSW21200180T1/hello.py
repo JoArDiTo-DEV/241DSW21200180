ce6e4079db243e761efdb623629a7c38fb8552b3
JoArDiTo-DEV <sm.armando05@gmail.com>
2024-04-10 13:32:29 -0500

    3e

--- 241DSW21200180T1/hello.py
+++ 241DSW21200180T1/hello.py
@@ -1,11 +1,16 @@
-#COMMIT 3d
+#COMMIT 3e
 
+#Se tiene que instalar la librería flask_moment con el comando pip
+
+from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.errorhandler(404)
@@ -20,7 +25,7 @@ def internal_server_error(e):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -28,7 +33,8 @@ def user(name):
     return render_template('user.html', name=name)
 
 
+
 if __name__ == '__main__':
     app.run(debug=True)
 
-#EL SIGUIENTE COMMIT ES EL 3e
+
